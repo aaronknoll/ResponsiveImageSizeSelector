f034ba6a62f7cabaa9bfeb5986c12f22aca7d174
aaronknoll <Aaron.Knoll@gmail.com>
2012-02-24 11:15:05 -0500

    Sort of working.

--- plugin.php
+++ plugin.php
@@ -1,42 +1,29 @@
 <?php
-add_plugin_hook('install', 'SimpleVocabPlugin::install');
-add_plugin_hook('uninstall', 'SimpleVocabPlugin::uninstall');
-add_plugin_hook('initialize', 'SimpleVocabPlugin::initialize');
 
-add_filter('admin_navigation_main', 'SimpleVocabPlugin::adminNavigationMain');
+add_plugin_hook('initialize', 'ResponsiveImageSelector::initialize');
+add_filter('admin_navigation_main', 'ResponsiveImageSelector::addToNav');
 
-class SimpleVocabPlugin
+
+class ResponsiveImageSelector
 {
-    public static function install()
-    {
-        $db = get_db();
-        $sql = "
-        CREATE TABLE `{$db->prefix}simple_vocab_terms` (
-            `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-            `element_id` int(10) unsigned NOT NULL,
-            `terms` text COLLATE utf8_unicode_ci NOT NULL,
-            PRIMARY KEY (`id`),
-            UNIQUE KEY `element_id` (`element_id`)
-        ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
-        $db->query($sql);
-    }
-    
-    public static function uninstall()
-    {
-        $db = get_db();
-        $sql = "DROP TABLE IF EXISTS `{$db->prefix}simple_vocab_terms`;";
-        $db->query($sql);
-    }
-    
+
     public static function initialize()
     {
-        $front = Zend_Controller_Front::getInstance();
-        $front->registerPlugin(new SimpleVocab_Controller_Plugin_SelectFilter);
+      $front = Zend_Controller_Front::getInstance();
+      $front->registerPlugin(new ResponsiveImages_Controller_Plugin_SelectFilter);
     }
-    
-    public static function adminNavigationMain($nav)
+	
+    public static function addToNav($nav)
     {
-        $nav['Simple Vocab'] = uri('simple-vocab');
+        $nav['Adv Image Options'] = uri('responsive-images');
         return $nav;
     }
+}
+
+class ResponsiveImages_Controller_Plugin_SelectFilter extends Zend_Controller_Plugin_Abstract
+{
+    public function preDispatch(Zend_Controller_Request_Abstract $request)
+    {
+        $db = get_db();
+	}
 }
